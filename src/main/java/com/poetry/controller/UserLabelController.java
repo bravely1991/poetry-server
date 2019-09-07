6cf748dbec64e3dd14fcd45ad13fbf9a9162a655
bravely1991 <bravely1991@icloud.com>
2019-09-07 20:29:28 +0800

    新增标签创建、添加、创建并添加接口

--- src/main/java/com/poetry/controller/UserLabelController.java
+++ src/main/java/com/poetry/controller/UserLabelController.java
@@ -31,10 +31,54 @@ public class UserLabelController {
         }
     }
 
-    @RequestMapping(value="labelAdd", method={RequestMethod.GET, RequestMethod.POST})
-    public Response<String> labelAdd(LabelRequestDTO labelRequestDTO) {
+    @RequestMapping(value="labelCreate", method={RequestMethod.GET, RequestMethod.POST})
+    public Response<String> labelCreate(LabelRequestDTO labelRequestDTO) {
 
         Boolean isSuccess = userLabelService.saveLabel(labelRequestDTO.getUserId(), labelRequestDTO.getLabelName());
+
+        if (isSuccess == true) {
+            return Response.ok("Success");
+        } else {
+            return Response.error();
+        }
+    }
+
+    @RequestMapping(value="LabelAdd", method={RequestMethod.GET, RequestMethod.POST})
+    public Response<String> LabelAdd(LabelRequestDTO labelRequestDTO) {
+
+        Boolean isSuccess = userLabelService.savePoemLabel(labelRequestDTO.getUserId(), labelRequestDTO.getContentId(), labelRequestDTO.getLabelId());
+
+        if (isSuccess == true) {
+            return Response.ok("Success");
+        } else {
+            return Response.error();
+        }
+    }
+
+    @RequestMapping(value="LabelCreateAndAdd", method={RequestMethod.GET, RequestMethod.POST})
+    public Response<String> LabelCreateAndAdd(LabelRequestDTO labelRequestDTO) {
+
+        Boolean isCreateSuccess = userLabelService.saveLabel(labelRequestDTO.getUserId(), labelRequestDTO.getLabelName());
+
+        if (isCreateSuccess == true) {
+            Boolean isAddSuccess = userLabelService.savePoemLabel(labelRequestDTO.getUserId(), labelRequestDTO.getContentId(), labelRequestDTO.getLabelId());
+
+            if (isAddSuccess == true) {
+                return Response.ok("Success");
+            } else {
+                // 删除新增的标签 事务统一
+                return Response.error();
+            }
+        } else {
+            return Response.error();
+        }
+    }
+
+    // 未完成
+    @RequestMapping(value="LabelRemove", method={RequestMethod.GET, RequestMethod.POST})
+    public Response<String> LabelRemove(LabelRequestDTO labelRequestDTO) {
+
+        Boolean isSuccess = userLabelService.savePoemLabel(labelRequestDTO.getUserId(), labelRequestDTO.getContentId(), labelRequestDTO.getLabelId());
 
         if (isSuccess == true) {
             return Response.ok("Success");
